8ac42cc87ac1fc7f4acff41cde5e20055c98a577
AmandaRezquita <amandarezquita1969@gmail.com>
2024-12-13 11:12:23 +0700

    feat/teacher_number [AR]

--- database/migrations/2024_10_09_053558_create_teachers_table.php
+++ database/migrations/2024_10_09_053558_create_teachers_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string("fullname");
             $table->string("nickname");
             $table->string("birth_date");
+            $table->string("teacher_number");
             $table->foreignId('gender_id')->constrained('teacher_genders');
             $table->string("phone_number");
             $table->string("email")->unique();
