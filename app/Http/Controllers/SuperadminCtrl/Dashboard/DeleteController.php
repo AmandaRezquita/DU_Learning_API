8fa3b961a2f7ae97f82bc5fe5a4e514beadf8371
AmandaRezquita <amandarezquita1969@gmail.com>
2024-12-19 15:30:09 +0700

    feat/delete-class-id [AR]

--- app/Http/Controllers/SuperadminCtrl/Dashboard/DeleteController.php
+++ app/Http/Controllers/SuperadminCtrl/Dashboard/DeleteController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\SuperadminCtrl\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Principal\Auth\Principal;
 use App\Models\Student\Auth\Student;
+use App\Models\Superadmin\Dashboard\ClassSubject;
+use App\Models\Superadmin\Dashboard\SchoolClass;
+use App\Models\Superadmin\Dashboard\StudentClass;
+use App\Models\Superadmin\Dashboard\subjectaddTeacher;
 use App\Models\Teacher\Auth\Teacher;
 use Illuminate\Http\Request;
 
@@ -85,6 +89,110 @@ class DeleteController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => 'Principal account deleted successfully',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function deleteClass(Request $request, $id)
+    {
+        try {
+            $class = SchoolClass::find($id);
+
+            if (!$class) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Class not found'
+                ], 401);
+            }
+
+            $class->delete();
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Class deleted successfully',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function deleteTeacherClass(Request $request, $id)
+    {
+        try {
+            $class = subjectaddTeacher::find($id);
+
+            if (!$class) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Teacher not found'
+                ], 401);
+            }
+
+            $class->delete();
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Teacher deleted successfully',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function deleteStudentClass(Request $request, $id)
+    {
+        try {
+            $class = StudentClass::find($id);
+
+            if (!$class) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Student not found'
+                ], 401);
+            }
+
+            $class->delete();
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Student deleted successfully',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function deleteClassSubject(Request $request, $id)
+    {
+        try {
+            $class = ClassSubject::find($id);
+
+            if (!$class) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Subject not found'
+                ], 401);
+            }
+
+            $class->delete();
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Subject deleted successfully',
             ], 200);
         } catch (\Throwable $th) {
             return response()->json([
